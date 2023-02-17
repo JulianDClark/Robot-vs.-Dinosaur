276eb2addfef72ccd189e986c7eb72f455a7c1e7
JulianDClark <julianclark30153@gmail.com>
2023-02-17 16:47:00 -0600

    changes in code

--- Dinosaur.py
+++ Dinosaur.py
@@ -7,16 +7,18 @@ class dinosaur:
      self.attacks = ['Tail Whip']
      self.create_attack()
 
-     def create_attack(self):
+     def create_attacks(self):
         user_input = ''
         while user_input != '2':
-         user_input = input(f'enter a new attack for{self.name}:')
+            user_input = input(f'Enter A new attack for {self.name}. or press 2 to exit')
+        user_input = input(f'enter a new attack for{self.name}:')
         if user_input != '2':
             self.attacks.append(user_input)
+            print(f'successfully added {user_input} to attacks')
         print(f'final attack list for {self.name}:')
         print(self.attacks)
         print('')
         def attack(self,other_class):
-         damage = random.randit(10,20)
-         random_attack = random.choice(self.attacks)
-        
+            pass
+     damage = random.randint(10,20)
+     random_attack = random.choice(self.attacks)
