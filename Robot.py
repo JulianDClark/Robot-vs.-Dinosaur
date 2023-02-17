35339813e78df7fa3938c50cddca49b08d655591
JulianDClark <julianclark30153@gmail.com>
2023-02-17 15:33:42 -0600

    changes were made to files

---
+++ Robot.py
@@ -0,0 +1,21 @@
+import random
+class robot:
+    def __init__(self, name):
+     self.name = name
+     self.health = 100
+     self.attacks = ['electric shock']
+     self.create_attack()
+
+     def create_attack(self):
+        user_input = ''
+        while user_input != '2':
+         user_input = input(f'enter a new attack for{self.name}:')
+        if user_input != '2':
+            self.attacks.append(user_input)
+        print(f'final attack list for {self.name}:')
+        print(self.attacks)
+        print('')
+        def attack(self,other_class):
+         damage = random.randit(10,20)
+         random_attack = random.choice(self.attacks)
+
